64a635dd914ff49d32e0530e3e9ea1d5af89c2a8
Jennifer Scales <jennifer4scales@gmail.com>
2014-11-03 22:22:53 -0600

    add article_params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    @article = Article.new(article_params)
     #render plain: params[:article].inspect
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :topic, :text)
   end
 
 
