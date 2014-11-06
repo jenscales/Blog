63ee8a0ba2d79bd6a92925a426931a39d48e5522
Jennifer Scales <jennifer4scales@gmail.com>
2014-11-05 22:33:04 -0600

    add edit to articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,7 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def create
@@ -18,6 +19,10 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
     @article = Article.find(params[:id])
   end
 
